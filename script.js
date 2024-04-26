3f7edb44d89b3f1ad716e419d8ac50f93d5bc8ac
mtomardev <120013813+mtomardev@users.noreply.github.com>
2024-04-26 18:35:42 +0530

    first

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+let a = parseInt(prompt("Enter first value")) 
+let b = parseInt(prompt("Enter Second value")) 
+
+let a1 = a*2
+let ans = a1+b 
+
+alert("Fincal result : " + ans)
